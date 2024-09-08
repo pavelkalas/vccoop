9492141937d43cc33cea7f85e254300833223021
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-08 22:19:00 +0200

    Added logics to Automation class, tested, and works

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VcCoop.src.utils;
 
 namespace VcCoop
 {
@@ -10,6 +12,12 @@ namespace VcCoop
     {
         static void Main(string[] args)
         {
+            int processId = Process.GetProcesses().Where(p => p.MainWindowTitle.EndsWith("5425")).Select(p => p.Id).FirstOrDefault();
+
+            Automation auto = new Automation(processId);
+
+            auto.InsertToQueue("Hello");
+
         }
     }
 }
--- src/utils/Automation.cs
+++ src/utils/Automation.cs
@@ -1,12 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace VcCoop.src.utils
 {
     internal class Automation
     {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)] private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)] private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto)] private static extern int SendMessage(IntPtr hWnd, int msg, int wParam, StringBuilder lParam);
+        
+        [DllImport("user32.dll", SetLastError = true)] private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+        
+        [DllImport("kernel32.dll", SetLastError = true)] private static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+        
+        [DllImport("kernel32.dll", SetLastError = true)] private static extern bool CloseHandle(IntPtr hObject);
+        
+        [DllImport("user32.dll")] [return: MarshalAs(UnmanagedType.Bool)] private static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        
+        [DllImport("user32.dll")] private static extern IntPtr SetFocus(IntPtr hWnd);
+        
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)] private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        /// <summary>
+        /// Main GUI window handle.
+        /// </summary>
+        private static IntPtr windowHandle;
+
+        /// <summary>
+        /// Process ID
+        /// </summary>
+        private readonly int processId;
+
+        /// <summary>
+        /// Process instance
+        /// </summary>
+        private readonly Process processInstance;
+
+        /// <summary>
+        /// Queue of messages
+        /// </summary>
+        private static readonly Queue<string> messages = new Queue<string>();
+
+        public Automation(int pID)
+        {
+            Process proc = Process.GetProcesses()
+                .Where(p => p.Id == pID)
+                .FirstOrDefault();
+
+            if (proc.Id > 0 && !proc.HasExited)
+            {
+                processInstance = proc;
+                windowHandle = proc.MainWindowHandle;
+                processId = proc.Id;
+
+                new Thread(RunQueueWatcher).Start();
+            }
+        }
+
+        /// <summary>
+        /// Dequeues the queue and send output into process
+        /// </summary>
+        private void RunQueueWatcher()
+        {
+            while (true)
+            {
+                if (messages.Count > 0)
+                {
+                    SendInput(messages.Dequeue(), "Edit");
+                }
+
+                Thread.Sleep(20);
+            }
+        }
+
+        /// <summary>
+        /// Inserts a message into queue.
+        /// </summary>
+        /// <param name="message">Message to queue</param>
+        public void InsertToQueue(string message)
+        {
+            messages.Enqueue(message);
+        }
+
+        /// <summary>
+        /// Sends input to a window's specified GUI element.
+        /// </summary>
+        /// <param name="content">Text to send</param>
+        /// <param name="element">Element name (like textbox, listview, etc..)</param>
+        private void SendInput(string content, string element)
+        {
+            IntPtr textBoxHandle = FindWindowEx(windowHandle, IntPtr.Zero, element, null);
+
+            GetWindowThreadProcessId(textBoxHandle, out _);
+
+            SetFocus(textBoxHandle);
+
+            SendMessage(textBoxHandle, 0x000C, 0, new StringBuilder(content));
+            SendMessage(textBoxHandle, 513, 0, null);
+            SendMessage(textBoxHandle, 514, 0, null);
+            PostMessage(textBoxHandle, 256, (IntPtr)13, IntPtr.Zero);
+            PostMessage(textBoxHandle, 257, (IntPtr)13, IntPtr.Zero);
+        }
     }
 }
