e76f84dad37d3ed6b4b35316ddd129871675f365
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-09 15:13:11 +0200

    Added Entity manager class for listening all players information, added basic timer for loops, tested and works.

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VcCoop.src.managers;
 using VcCoop.src.utils;
 
 namespace VcCoop
@@ -12,12 +13,14 @@ namespace VcCoop
     {
         static void Main(string[] args)
         {
-            int processId = Process.GetProcesses().Where(p => p.MainWindowTitle.EndsWith("5425")).Select(p => p.Id).FirstOrDefault();
+            // int processId = Process.GetProcesses().Where(p => p.MainWindowTitle.EndsWith("5425")).Select(p => p.Id).FirstOrDefault();
 
-            Automation auto = new Automation(processId);
+            EntityManager em = new EntityManager(1000);
 
-            auto.InsertToQueue("Hello");
+            em.StartTask();
 
+            // to avoid closing application
+            Console.ReadLine();
         }
     }
 }
---
+++ src/managers/EntityManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VcCoop.src.managers
+{
+    internal class EntityManager
+    {
+        /// <summary>
+        /// Interval to delay next check step.
+        /// </summary>
+        private readonly int checkInterval;
+
+        /// <summary>
+        /// Main task timer
+        /// </summary>
+        private Timer taskTimer;
+
+        public EntityManager(int checkInterval)
+        {
+            this.checkInterval = (checkInterval >= 1000) ? checkInterval : 1000;
+            
+        }
+
+        /// <summary>
+        /// Start the task.
+        /// </summary>
+        public void StartTask()
+        {
+            taskTimer = new Timer(Task, null, 0, checkInterval);
+        }
+
+        /// <summary>
+        /// Stop the running task.
+        /// </summary>
+        public void StopTask()
+        {
+            taskTimer.Change(Timeout.Infinite, Timeout.Infinite);
+            taskTimer.Dispose();
+        }
+
+        /// <summary>
+        /// Main task method.
+        /// </summary>
+        private void Task(Object obj)
+        {
+            // just for testing case
+            Console.WriteLine("tick!");
+        }
+    }
+}
