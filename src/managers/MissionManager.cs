92e2094e4f567c34f274abe0772933c69e590d5c
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-10 16:01:41 +0200

    Added structure for Players and Enemy for saving informations, added logic for filtering informations

--- src/managers/MissionManager.cs
+++ src/managers/MissionManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using VcCoop.src.models;
@@ -11,6 +13,36 @@ namespace VcCoop.src.managers
 {
     internal class MissionManager
     {
+        /// <summary>
+        /// Enemy info structure
+        /// </summary>
+        struct Enemy
+        {
+            public int Live;
+            public int Dead;
+            public int Total;
+        }
+
+        /// <summary>
+        /// Player info structure
+        /// </summary>
+        struct Player
+        {
+            public int Live;
+            public int Dead;
+            public int Total;
+        }
+
+        /// <summary>
+        /// Player structure instance
+        /// </summary>
+        Player player = new Player();
+
+        /// <summary>
+        /// Enemy structure instance
+        /// </summary>
+        Enemy enemy = new Enemy();
+
         /// <summary>
         /// Automation instance
         /// </summary>
@@ -23,6 +55,27 @@ namespace VcCoop.src.managers
 
         public void OnDataTick(List<Entity> entities)
         {
+            // collect informations about players
+            player.Total = entities.Where(e => e is models.Player).Count();
+            player.Live = entities.Where(e => e is models.Player && e.Deaths == 0).Count();
+            player.Dead = entities.Where(e => e is models.Player && e.Deaths > 0).Count();
+
+            // collect informations about enemies
+            enemy.Total = entities.Where(e => e is models.Enemy).Count();
+            enemy.Live = entities.Where(e => e is models.Enemy && e.Deaths == 0).Count();
+            enemy.Dead = entities.Where(e => e is models.Enemy && e.Deaths > 0).Count();
+
+            // just print out for test
+            Console.Clear();
+            Console.WriteLine("Players:");
+            Console.WriteLine(" Total: " + player.Total);
+            Console.WriteLine(" Alive: " + player.Live);
+            Console.WriteLine("  Dead: " + player.Dead);
+
+            Console.WriteLine("\nEnemy:");
+            Console.WriteLine(" Total: " + enemy.Total);
+            Console.WriteLine(" Alive: " + enemy.Live);
+            Console.WriteLine("  Dead: " + enemy.Dead);
         }
     }
 }
