e1ccdf031171d84656279af766ebc890ef44d154
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-10 16:12:25 +0200

    Removed Timer and replaced with WHILE cycle, because we need to WAIT for the CALL function from MissionManager ends

--- src/managers/EntityManager.cs
+++ src/managers/EntityManager.cs
@@ -18,11 +18,6 @@ namespace VcCoop.src.managers
         private readonly int checkInterval;
 
         /// <summary>
-        /// Main task timer
-        /// </summary>
-        private Timer taskTimer;
-
-        /// <summary>
         /// Automation instance
         /// </summary>
         private Automation automation;
@@ -31,6 +26,11 @@ namespace VcCoop.src.managers
         /// Event handler instance
         /// </summary>
         private EventsHandler eventsHandler;
+
+        /// <summary>
+        /// Controls when thread should start and when stop
+        /// </summary>
+        private bool taskRunning;
 
         public EntityManager(int checkInterval, Automation automation)
         {
@@ -44,7 +44,8 @@ namespace VcCoop.src.managers
         /// </summary>
         public void StartTask()
         {
-            taskTimer = new Timer(Task, null, 0, checkInterval);
+            taskRunning = true;
+            new Thread(Task).Start();
         }
 
         /// <summary>
@@ -52,81 +53,85 @@ namespace VcCoop.src.managers
         /// </summary>
         public void StopTask()
         {
-            taskTimer.Change(Timeout.Infinite, Timeout.Infinite);
-            taskTimer.Dispose();
+            taskRunning = false;
         }
 
         /// <summary>
         /// Main task method.
         /// </summary>
-        private void Task(Object obj)
+        private void Task()
         {
-            // clears the listbox (to get only "list" command data)
-            automation.SetEmptyElement("ListBox");
-
-            // sends list to vcguard window, to retrieve player data
-            automation.InsertToQueue("list");
-
-            // wait some time to make sure all data from LIST are retrieved
-            Thread.Sleep(200);
-
-            // get all data from console
-            List<string> rawData = new List<string>(automation.GetElementData("ListBox"));
-
-            // returns it when nobody is on server.
-            // (this basically can happen when server crash, so server can be empty)
-            if (rawData == null)
+            while (taskRunning)
             {
-                return;
-            }
+                // clears the listbox (to get only "list" command data)
+                automation.SetEmptyElement("ListBox");
 
-            // filtered data from console (selected only players line, removed the time in front of each string)
-            rawData = rawData
-                .Where(ent => ent.Contains("kills:") && ent.Contains("deaths:") && ent.Contains("ping:") && ent.Length > 10) // select only content which contains kills, deaths and ping string
-                .Select(ent => ent.Substring(10).Trim()) // remove the time in front of each entry
-                .ToList(); // cast to list
+                // sends list to vcguard window, to retrieve player data
+                automation.InsertToQueue("list");
 
-            // use regular expression to extract kills, deaths, and ping values
-            var regex = new Regex(@"kills:\s*(\d+)\s*deaths:\s*(\d+)\s*ping:\s*(\d+)");
+                // wait some time to make sure all data from LIST are retrieved
+                Thread.Sleep(500);
 
-            // Parse the data like name, kills, deaths and ping
-            List<Entity> entityList = rawData
-                .Select(entry =>
+                // get all data from console
+                List<string> rawData = new List<string>(automation.GetElementData("ListBox"));
+
+                // returns it when nobody is on server.
+                // (this basically can happen when server crash, so server can be empty)
+                if (rawData == null)
                 {
-                    string playerData = entry.Split(']')[1].Trim();
+                    return;
+                }
 
-                    string name = playerData.Split(new[] { ' ' }, 2)[0];
+                // filtered data from console (selected only players line, removed the time in front of each string)
+                rawData = rawData
+                    .Where(ent => ent.Contains("kills:") && ent.Contains("deaths:") && ent.Contains("ping:") && ent.Length > 10) // select only content which contains kills, deaths and ping string
+                    .Select(ent => ent.Substring(10).Trim()) // remove the time in front of each entry
+                    .ToList(); // cast to list
 
-                    string statsData = playerData.Substring(name.Length).Trim();
+                // use regular expression to extract kills, deaths, and ping values
+                var regex = new Regex(@"kills:\s*(\d+)\s*deaths:\s*(\d+)\s*ping:\s*(\d+)");
 
-                    var match = regex.Match(statsData);
-
-                    if (match.Success)
+                // Parse the data like name, kills, deaths and ping
+                List<Entity> entityList = rawData
+                    .Select(entry =>
                     {
-                        int kills = int.Parse(match.Groups[1].Value);
-                        int deaths = int.Parse(match.Groups[2].Value);
-                        int ping = int.Parse(match.Groups[3].Value);
+                        string playerData = entry.Split(']')[1].Trim();
 
-                        // if IS ping equal to ZERO, it's NPC, not a player
-                        //
-                        // (but sometimes it can cause confusion, because
-                        // when a player join a server, it shows ping 0
-                        // for some little time.. until it establish)
-                        if (ping == 0)
+                        string name = playerData.Split(new[] { ' ' }, 2)[0];
+
+                        string statsData = playerData.Substring(name.Length).Trim();
+
+                        var match = regex.Match(statsData);
+
+                        if (match.Success)
                         {
-                            return new Enemy(name, kills, deaths);
-                        }
-                        else
-                        {
-                            return new Player(name, kills, deaths, ping);
-                        }
-                    }
+                            int kills = int.Parse(match.Groups[1].Value);
+                            int deaths = int.Parse(match.Groups[2].Value);
+                            int ping = int.Parse(match.Groups[3].Value);
 
-                    return new Entity(null, 0, 0);
-                })
-                .ToList();
+                            // if IS ping equal to ZERO, it's NPC, not a player
+                            //
+                            // (but sometimes it can cause confusion, because
+                            // when a player join a server, it shows ping 0
+                            // for some little time.. until it establish)
+                            if (ping == 0)
+                            {
+                                return new Enemy(name, kills, deaths);
+                            }
+                            else
+                            {
+                                return new Player(name, kills, deaths, ping);
+                            }
+                        }
 
-            eventsHandler.OnEntityListUpdate(entityList);
+                        return new Entity(null, 0, 0);
+                    })
+                    .ToList();
+
+                eventsHandler.OnEntityListUpdate(entityList);
+
+                Thread.Sleep(checkInterval);
+            }
         }
     }
 }
