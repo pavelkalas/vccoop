091cdc68a2f1539fcd8d0c48e7c9476e812438d6
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-11 21:10:40 +0200

    Added some mission logics to code

--- src/managers/MissionManager.cs
+++ src/managers/MissionManager.cs
@@ -5,6 +5,7 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Permissions;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using VcCoop.src.models;
 using VcCoop.src.utils;
@@ -48,6 +49,16 @@ namespace VcCoop.src.managers
         /// </summary>
         private Automation automation;
 
+        /// <summary>
+        /// Maximum mission attempts
+        /// </summary>
+        private const int maxAttempts = 3;
+
+        /// <summary>
+        /// Actual mission attempts
+        /// </summary>
+        private int currentMissionAttempt = 1;
+
         public MissionManager(Automation automation)
         {
             this.automation = automation;
@@ -65,17 +76,68 @@ namespace VcCoop.src.managers
             enemy.Live = entities.Where(e => e is models.Enemy && e.Deaths == 0).Count();
             enemy.Dead = entities.Where(e => e is models.Enemy && e.Deaths > 0).Count();
 
-            // just print out for test
-            Console.Clear();
-            Console.WriteLine("Players:");
-            Console.WriteLine(" Total: " + player.Total);
-            Console.WriteLine(" Alive: " + player.Live);
-            Console.WriteLine("  Dead: " + player.Dead);
+            // do logic only when is some player on server
+            if (player.Total > 0)
+            {
+                if (player.Dead > 0 && enemy.Dead == 0)
+                {
+                    automation.InsertToQueue("adminsay \"Starting the mission attempt...\"");
 
-            Console.WriteLine("\nEnemy:");
-            Console.WriteLine(" Total: " + enemy.Total);
-            Console.WriteLine(" Alive: " + enemy.Live);
-            Console.WriteLine("  Dead: " + enemy.Dead);
+                    Thread.Sleep(2500);
+
+                    for (int i = 0; i < 2; i++)
+                    {
+                        automation.InsertToQueue("restartmap");
+                        Thread.Sleep(1000);
+                    }
+                }
+                else if (player.Dead > 0 && enemy.Dead > 0)
+                {
+                    // first attempt failed
+                    if (currentMissionAttempt == 1)
+                    {
+                        automation.InsertToQueue("adminsay \"Mission failed! Try again..\"");
+                    }
+
+                    // second attempt failed
+                    else if (currentMissionAttempt == maxAttempts - 1)
+                    {
+                        automation.InsertToQueue("adminsay \"Mission failed! This is your last cahnce..\"");
+                    }
+
+                    // third attempt failed (last)
+                    else if (currentMissionAttempt == maxAttempts)
+                    {
+                        automation.InsertToQueue("adminsay \"Last attempt failed, switching to nextmap\"");
+                    }
+
+                    if (currentMissionAttempt != maxAttempts)
+                    {
+                        Thread.Sleep(2500);
+
+                        for (int i = 0; i < 2; i++)
+                        {
+                            automation.InsertToQueue("restartmap");
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    else
+                    {
+                        Thread.Sleep(2500);
+
+                        // switch the map
+                        automation.InsertToQueue("nextmap");
+                        
+                        // resets the attempt count to 1
+                        currentMissionAttempt = 1;
+
+                        // do mission stats record..
+                        // ...
+                    }
+
+                    currentMissionAttempt++;
+                }
+            }
         }
     }
 }
