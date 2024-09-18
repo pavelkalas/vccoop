78a6f9d4b0d6dd52b4908f5f3aecc89e8c095d13
Pavel Kalaš <pavelkalas2048@seznam.cz>
2024-09-18 23:30:30 +0200

    Added content directory and dynamic strings list

---
+++ src/content/Strings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace VcCoop.src.content
+{
+    internal class Strings
+    {
+        /// <summary>
+        /// List of the strings
+        /// </summary>
+        private static Dictionary<int, string> stringList = new Dictionary<int, string>();
+
+        /// <summary>
+        /// Load the strings into dictionary list
+        /// </summary>
+        public static void Load()
+        {
+        }
+    }
+}
